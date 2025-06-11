548ae515940d59d4c7fcb3f426b618727e41c11c
hek316 <yhr05008@naver.com>
2025-06-11 22:19:59 +0900

    @ConfigurationProperties 에  Bean Validation  적용

--- src/main/java/hello/ExternalReadApplication.java
+++ src/main/java/hello/ExternalReadApplication.java
@@ -2,12 +2,13 @@ package hello;
 
 import hello.config.MyDataSourceConfigV1;
 import hello.config.MyDataSourceConfigV2;
+import hello.config.MyDataSourceConfigV3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Import;
 
-@Import(MyDataSourceConfigV2.class)
+@Import(MyDataSourceConfigV3.class)
 @SpringBootApplication(scanBasePackages = "hello.datasource")
 public class ExternalReadApplication {
 
